849d737ba628e23fde8fc9adc104186b63d69efe
hong2HSH <tnghd1717@gmail.com>
2023-08-09 00:56:37 +0900

    230808

--- Cook/Cook/urls.py
+++ Cook/Cook/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("accounts.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("", RedirectView.as_view(url="accounts/", permanent=True)),
 ]
--- Cook/accounts/views.py
+++ Cook/accounts/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from django.db import transaction
+from django.views.decorators.csrf import csrf_exempt
+from argon2 import PasswordHasher
+from .forms import RegisterForm
 
 # Create your views here.
 
 
 def login(request):
     return render(request, "accounts/login.html", {})
+
+
+@csrf_exempt
+def register(request):
+    register_form = RegisterForm()
+    context = {"forms": register_form}
+
+    if request.method == "GET":
+        return render(request, "accounts/register.html", context)
